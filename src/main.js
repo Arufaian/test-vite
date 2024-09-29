cb2aa834ff746a80cb1c4a35302f4ca5c8bfc6fe
Alfian <alfianilyasya13@gmail.com>
2024-09-29 18:19:55 +0700

    add some serious

--- src/main.js
+++ src/main.js
@@ -1,24 +1,71 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./css/index.css";
+import anime from "animejs/lib/anime.es.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const header = document.querySelector("#header");
+const nggakMau = document.querySelector("#nggak");
+const mau = document.querySelector("#mau");
+const main = document.querySelector("#main-section");
 
-setupCounter(document.querySelector('#counter'))
+let buttonField = document.querySelector("#button-field");
+let failure = 0;
+
+nggakMau.addEventListener("click", function () {
+  const width = buttonField.offsetWidth;
+  const height = buttonField.offsetHeight;
+
+  let randomX = Math.floor(Math.random() * (width * 0.5)) + 1;
+  let randomY = Math.floor(Math.random() * (height * 0.5)) + 1;
+
+  playAudio("nggak");
+
+  anime({
+    targets: nggakMau,
+    translateX: randomX,
+    translateY: randomY,
+    easing: "easeOutElastic(1, .3",
+    duration: 1000,
+  });
+  failure += 1;
+
+  if (failure == 4) {
+    nggakMau.textContent = "jangan dimainin atu";
+  } else if (failure == 13) {
+    nggakMau.textContent = "Dih jahat banget";
+  } else if (failure == 17) {
+    nggakMau.textContent = "hiks hiks";
+  } else if (failure > 17) {
+    document.location.href =
+      "https://www.w3schools.com/jsref/prop_loc_href.asp";
+  }
+
+  console.log(failure);
+});
+
+mau.addEventListener("click", function () {
+  header.textContent = "Makasiiiiih";
+  main.innerHTML = printMainSection();
+  buttonField.classList.add("hidden");
+});
+
+function printMainSection() {
+  playAudio("saveNsound");
+  return `<div class="w-full flex justify-center">
+          <div class="p-8 max-w-xl">
+            <img
+              id="image"
+              src="../public/heart.gif"
+              alt=""
+              class="mb-8"
+            />
+            <p class="py-8 text-center text-base font-medium text-slate-600">
+              Emmm, makasiih. Aku jujur nggak tau mau bilang apa lagi, tapi makasih udah terima aku, ehe. 
+              Dah ah, sakit pinggang anjir.
+            </p>
+          </div>
+        </div>`;
+}
+
+function playAudio(song) {
+  let audio = new Audio(`../public/music/${song}.mp3`);
+  audio.play();
+}
--- src/wanjay.js
+++
@@ -1,8 +0,0 @@
-import anime from "animejs/lib/anime.es.js";
-
-anime({
-    targets: '.kotak',
-    translateX: 250,
-    rotate: '1turn',
-    duration: 10000
-  });
